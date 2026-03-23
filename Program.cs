835629069d29a3a5d9d1e50613228f1546cf9cd3
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-03-23 07:25:13 +0545

    2d-array acess methods

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿///2D-ArraY
+using System;
+class Calculator{
+    int[,] numbers = {{10,30,40},{
+        50,60,70
+    }};
+    int sum =0;
+    int max =0;
+
+    static void Main(string[] args)
+    {
+        Calculator cal = new Calculator();
+        foreach(int num in cal.numbers){
+
+            Console.WriteLine(num);
+        
+        }
+        Console.WriteLine("Max="+cal.max);
+    }
+   
+}
+
