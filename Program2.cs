6bb74f15914a3eb83c42fc5e85a4ca243d0c3311
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-03-24 07:00:32 +0545

    Display Method calling

---
+++ Program2.cs
@@ -0,0 +1,9 @@
+using System;
+class Hello{
+   static void Display(string name){
+        Console.WriteLine("Hello"+name);
+    }
+     static void Main(string[] args){
+        Display("Kasam");
+     }
+}
