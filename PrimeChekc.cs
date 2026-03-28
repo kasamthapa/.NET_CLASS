5edcc7905fac872123b6403c1e978d6a0dc2f866
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-03-28 23:26:30 +0545

    Prime number check problem

---
+++ PrimeChekc.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 29;
+        bool isPrime = true;
+
+        if (number <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine(number + " is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a prime number.");
+        }
+    }
+}
