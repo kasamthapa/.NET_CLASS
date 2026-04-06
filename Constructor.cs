e752818829741ffefdcb937dea1ccf2b58e2be11
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-04-06 07:22:05 +0545

    constructor problem

---
+++ Constructor.cs
@@ -0,0 +1,57 @@
+using System;
+
+class Car
+{
+
+    public string brand;
+    public int speed;
+    public static int carCount;
+
+
+    static Car()
+    {
+        carCount = 0;
+        Console.WriteLine("Static Constructor Called");
+    }
+
+
+    public Car()
+    {
+        brand = "Unknown";
+        speed = 0;
+        carCount++;
+        Console.WriteLine("Default Constructor Called");
+    }
+
+
+    public Car(string b, int s)
+    {
+        brand = b;
+        speed = s;
+        carCount++;
+        Console.WriteLine("Parameterized Constructor Called");
+    }
+
+
+    public void Display()
+    {
+        Console.WriteLine("Brand: " + brand + ", Speed: " + speed);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Car c1 = new Car();
+        c1.Display();
+
+
+        Car c2 = new Car("Toyota", 120);
+        c2.Display();
+
+        // Access static variable
+        Console.WriteLine("Total Cars: " + Car.carCount);
+    }
+}
