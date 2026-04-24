fea13e66db773ef2ce9cb796b04cae15e1237447
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-04-24 06:46:28 +0545

    IfStmt program

---
+++ IfStmt.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+    static void Main() {
+        int num = 10;
+
+        if (num > 5) {
+            Console.WriteLine("Number is greater than 5");
+        }
+    }
+}
