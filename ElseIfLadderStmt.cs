65d83dc4cb29f7db485790723514d0a971b9ec54
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-04-24 06:47:47 +0545

    ElseIf Ladder program

---
+++ ElseIfLadderStmt.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+    static void Main() {
+        int marks = 75;
+
+        if (marks >= 90) {
+            Console.WriteLine("Grade A");
+        } else if (marks >= 60) {
+            Console.WriteLine("Grade B");
+        } else {
+            Console.WriteLine("Grade C");
+        }
+    }
+}
