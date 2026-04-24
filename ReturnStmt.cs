0b79324849024a94df4d29f0131db1d11d318494
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-04-24 06:52:00 +0545

    Return Statement program

---
+++ ReturnStmt.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    static int Add(int a, int b) {
+        return a + b;
+    }
+
+    static void Main() {
+        int result = Add(3, 4);
+        Console.WriteLine(result);
+    }
+}
