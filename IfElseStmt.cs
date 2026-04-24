919dd480609a446bc3d8f4832ba31d636e87818c
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-04-24 06:47:01 +0545

    IfElseStmt program

---
+++ IfElseStmt.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+    static void Main() {
+        int num = 3;
+
+        if (num > 5) {
+            Console.WriteLine("Greater than 5");
+        } else {
+            Console.WriteLine("Less than or equal to 5");
+        }
+    }
+}
